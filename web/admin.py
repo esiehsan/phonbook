a7b73a13f18a3ce28907a4c69692fac1b74fcbd1
Ehsan <ehsan.mirshojaee@gmail.com>
2022-12-30 20:20:41 +0330

    making some small changes to display the table of person in admin view and allow entering empty fields of phone and email

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Person
 
 # Register your models here.
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('fName', 'lName')
+
+
+admin.site.register(Person, PersonAdmin)
--- web/models.py
+++ web/models.py
@@ -4,7 +4,10 @@ from django.db import models
 class Person(models.Model):
     fName = models.CharField(max_length=100)
     lName = models.CharField(max_length=100)
-    directPhone = models.CharField(max_length=8)
-    internalPhone = models.CharField(max_length=3)
-    mobilePhone = models.CharField(max_length=11)
-    email = models.EmailField(max_length=254)
+    directPhone = models.CharField(max_length=8, blank=True, verbose_name='مستقیم')
+    internalPhone = models.CharField(max_length=3, blank=True, verbose_name='داخلی')
+    mobilePhone = models.CharField(max_length=11, blank=True, verbose_name='همراه')
+    email = models.EmailField(max_length=254, blank=True, verbose_name='پست الکترونیک')
+
+    def __str__(self):
+        return self.fName + ' ' + self.lName
